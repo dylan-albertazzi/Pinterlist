fd3858cbd4c7f03b0651eac70b52b3c94d9aeef7
dylan-albertazzi <albertad@oregonstate.edu>
2020-05-26 21:48:58 -0700

    auth progress

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,33 @@
-import React from 'react';
-import AppNavbar from './components/AppNavbar';
-import GroceryList from './components/GroceryList';
-import ItemModal from './components/ItemModal';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { Container } from 'reactstrap';
+import React, { Component } from "react";
+import AppNavbar from "./components/AppNavbar";
+import GroceryList from "./components/GroceryList";
+import ItemModal from "./components/ItemModal";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Container } from "reactstrap";
 
+import { Provider } from "react-redux";
+import store from "./store";
+import { loadUser } from "./actions/authActions";
 
-import { Provider } from 'react-redux';
-import store from './store';
+import "./App.css";
 
-import './App.css';
-
-function App() {
-  return (
-    <Provider store={ store }>
-    <div className="App">
-      <AppNavbar/>
-      <Container>
-        <ItemModal/>
-        <GroceryList/>
-      </Container>
-    </div>
-    </Provider>
-  );
+class App extends Component {
+  componentDidMount() {
+    store.dispatch(loadUser());
+  }
+  render() {
+    return (
+      <Provider store={store}>
+        <div className="App">
+          <AppNavbar />
+          <Container>
+            <ItemModal />
+            <GroceryList />
+          </Container>
+        </div>
+      </Provider>
+    );
+  }
 }
 
 export default App;
---
+++ client/src/components/auth/RegisterModal.js
@@ -0,0 +1,86 @@
+import React, { Component } from "react";
+import {
+  Button,
+  Modal,
+  ModalHeader,
+  ModalBody,
+  Form,
+  FormGroup,
+  Label,
+  Input,
+  NavLink,
+} from "reactstrap";
+
+//Container - a component that is hooked to redux
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+
+class RegisterModal extends Component {
+  state = {
+    modal: false,
+    name: "",
+    email: "",
+    password: "",
+    msg: null,
+  };
+
+  static propTypes = {
+    isAuthenticated: PropTypes.bool,
+    error: PropTypes.object.isRequired,
+  };
+
+  toggle = () => {
+    this.setState({
+      modal: !this.state.modal,
+    });
+  };
+
+  onChange = (e) => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
+  onSubmit = (e) => {
+    e.preventDefault();
+
+    //close modal
+    this.toggle();
+  };
+
+  render() {
+    return (
+      <div>
+        <NavLink onClick={this.toggle} href="#">
+          Register
+        </NavLink>
+
+        <Modal isOpen={this.state.modal} toggle={this.toggle}>
+          <ModalHeader toggle={this.toggle}>Register</ModalHeader>
+          <ModalBody>
+            <Form onSubmit={this.onSubmit}>
+              <FormGroup>
+                <Label for="name">Name</Label>
+                <Input
+                  type="text"
+                  name="name"
+                  id="item"
+                  placeholder="Name"
+                  onChange={this.onChange}
+                ></Input>
+                <Button color="dark" style={{ marginTop: "2rem" }} block>
+                  Add Item
+                </Button>
+              </FormGroup>
+            </Form>
+          </ModalBody>
+        </Modal>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (state) => ({
+  isAuthenticated: state.auth.isAuthenticated,
+  error: state.error,
+});
+
+export default connect(mapStateToProps, {})(RegisterModal);
